a940510bb10b58198fc3f00581633d77aee8a71b
Daniel <r1912ramirez@gmail.com>
2021-01-27 11:32:33 -0600

    conexion

--- Controlador/corre_supervisor.php
+++ Controlador/corre_supervisor.php
@@ -14,12 +14,12 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 $mail->isSMTP();
 
-$correo= "";
-$Password= "";
+$correo= "vinculacionunah@informaticaunah.com";
+$Password= "Hola#*1234";
 $mail->SMTPDebug = 0;
-$mail->Host = 'SMTP.Office365.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'STARTTLS';
+$mail->Host = 'mail.informaticaunah.com';
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
 $mail->SMTPAuth = true;
 $mail->Username = $correo;
 $mail->Password = $Password;
@@ -50,12 +50,12 @@ function enviarEmailPracticante($cuerpo_estudiante,$asunto_estudiante,$ecorreo,$
 	
 	$mail->isSMTP();
 	
-	$correo= "";
-	$Password= "";
+	$correo= "vinculacionunah@informaticaunah.com";
+    $Password= "Hola#*1234";
 	$mail->SMTPDebug = 0;
-	$mail->Host = 'SMTP.Office365.com';
-	$mail->Port = 587;
-	$mail->SMTPSecure = 'STARTTLS';
+	$mail->Host = 'mail.informaticaunah.com';
+	$mail->Port = 465;
+	$mail->SMTPSecure = 'ssl';
 	$mail->SMTPAuth = true;
 	$mail->Username = $correo;
 	$mail->Password = $Password;
--- clases/global.php
+++ clases/global.php
@@ -3,13 +3,13 @@
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "automatizacion4");
+define("DB_NAME", "informat_automatizacion");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "admin");
+define("DB_USERNAME", "informat_admin");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "admin");
+define("DB_PASSWORD", "HLo{Q3e{)II^");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
