d4ef4994f09d109182ac5fdb17ab75fb971b446d
EdAl2019 <55669709+EdAl2019@users.noreply.github.com>
2021-02-02 14:52:24 -0600

    Modificar titulo login
    Se modifico titulo

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Informatica Admistrativa</title>
+      <title>Informatica</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- SweetAlert2 -->
