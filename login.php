06a0293c01d8bebe2701b02ad4606bd795c6fefd
Daniel <r1912ramirez@gmail.com>
2021-03-02 15:31:16 -0600

    prueba 3

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
            style="opacity: .8">
         </div>
 
-      <p class="login-box-msg">Iniciar Sesion</p>
+      <p class="login-box-msg">Iniciar </p>
 
 <form action="Controlador/existe_usuario_controlador.php" method="post">
         <div class="input-group mb-3">
