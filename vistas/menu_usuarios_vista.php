80bc68fa9f220ab817ddce0b1fd72de1120e9512
AnderMfl <62123028+AnderMfl@users.noreply.github.com>
2021-03-10 09:56:27 -0600

    Add files via upload

--- vistas/menu_usuarios_vista.php
+++ vistas/menu_usuarios_vista.php
@@ -27,6 +27,56 @@ $_SESSION['gestion_usuario_menu']="No
   tiene permisos para visualizar";
 
 }
+
+
+
+if (permiso_ver('11')=='1')
+ {
+  
+  $_SESSION['crear_personas_menu']="...";
+}
+else
+{
+$_SESSION['crear_personas_menu']="No 
+  tiene permisos para visualizar";
+
+}
+
+if (permiso_ver('10')=='1')
+ {
+  
+  $_SESSION['crear_estudiantes_menu']="...";
+}
+else
+{
+$_SESSION['crear_estudiantes_menu']="No 
+  tiene permisos para visualizar";
+
+}
+
+if (permiso_ver('10')=='1')
+ {
+  
+  $_SESSION['gestion_persona_menu']="...";
+}
+else
+{
+$_SESSION['gestion_persona_menu']="No 
+  tiene permisos para visualizar";
+
+}
+
+if (permiso_ver('10')=='1')
+ {
+  
+  $_SESSION['perfil_persona_menu']="...";
+}
+else
+{
+$_SESSION['perfil_persona_menu']="No 
+  tiene permisos para visualizar";
+
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -113,6 +163,103 @@ $_SESSION['gestion_usuario_menu']="No
       </div><!--/. container-fluid -->
       </div>
     </section>
+
+
+    <section class="content">
+        <div class="container-fluid">
+          <!-- Info boxes -->
+          <div class="row" style="  display: flex;
+    align-items: center;
+    justify-content: center;">
+
+
+
+            <div class="col-6 col-sm-6 col-md-4">
+              <div class="small-box bg-light">
+                <div class="inner">
+                  <h4>Registro de Personas </h4>
+                  <p><?php echo $_SESSION['crear_personas_menu']; ?></p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-user-plus"></i>
+                </div>
+                <a href="../vistas/registro_personas_vista.php" class="small-box-footer">
+                  Ir <i class="fas fa-arrow-circle-right"></i>
+                </a>
+              </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+
+            
+            <div class="col-6 col-sm-6 col-md-4">
+              <div class="small-box bg-light">
+                <div class="inner">
+                  <h4>Registro de Estudiantes </h4>
+                  <p><?php echo $_SESSION['crear_estudiantes_menu']; ?></p>
+                </div>
+                <div class="icon">
+                  <i class="fas fa-user-plus"></i>
+                </div>
+                <a href="../vistas/registro_estudiantes_vista.php" class="small-box-footer">
+                  Ir <i class="fas fa-arrow-circle-right"></i>
+                </a>
+              </div>
+              <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+
+
+            <!-- fix for small devices only -->
+    <div class="clearfix hidden-md-up"></div>
+
+      <!--  <div class="col-12 col-sm-6 col-md-4">
+<div class="small-box bg-primary">
+<div class="inner">
+<h4>Perfil </h4>
+<p><?php echo $_SESSION['perfil_persona_menu'];?></p>
+</div>
+<div class="icon">
+<i class="fas fa-user-edit"></i>
+</div>
+<a href="../vistas/perfil_personas_vista.php" class="small-box-footer">
+Ir <i class="fas fa-arrow-circle-right"></i>
+</a>
+</div>
+</div>-->
+
+         
+    <!-- fix for small devices only 
+    <div class="clearfix hidden-md-up"></div>
+
+<div class="col-12 col-sm-6 col-md-4">
+<div class="small-box bg-primary">
+<div class="inner">
+<h4>Gestión Personas </h4>
+<p><?php echo $_SESSION['gestion_persona_menu'];?></p>
+</div>
+<div class="icon">
+<i class="fas fa-user-edit"></i>
+</div>
+<a href="../vistas/gestion_personas_vista.php" class="small-box-footer">
+Ir <i class="fas fa-arrow-circle-right"></i>
+</a>
+</div>
+</div>-->
+
+            <!-- /.row -->
+      </div><!--/. container-fluid -->
+      </div>
+
+
+      
+
+
+
+           
+      </section>
+      
+
    <!-- /.content -->
   </div>
  
