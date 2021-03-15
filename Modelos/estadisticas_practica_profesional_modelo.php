cefa9afe8f1ccdaf5deb86544a3ab9998c604c48
Daniel <r1912ramirez@gmail.com>
2021-03-14 22:54:54 -0600

    conexion reportes

--- Modelos/estadisticas_practica_profesional_modelo.php
+++ Modelos/estadisticas_practica_profesional_modelo.php
@@ -2,6 +2,8 @@
 //Incluímos inicialmente la conexión a la base de datos
 require "../clases/conexion_mantenimientos.php";
 
+$instancia_conexion = new conexion();
+
 class Estadisticas
 {
 
@@ -14,7 +16,8 @@ class Estadisticas
      
         //implementamos un metodo para listar los registros
         public function listar()
-        {
+        {       
+                global $instancia_conexion;
                 $sql="SELECT px.valor, concat(a.nombres,' ',a.apellidos) as nombre, ep.nombre_empresa, ep.direccion_empresa, pe.docente_supervisor, pe.fecha_inicio, pe.fecha_finaliza, c.valor Correo, e.valor Celular, ep.tipo_empresa, ep.departamento_empresa, ep.jefe_inmediato, ep.titulo_jefe_inmediato, ep.cargo_jefe_inmediato, ep.correo_jefe_inmediato, ep.telefono_jefe_inmediato, ep.labora_dentro, a.id_persona, pe.horas
 
                 FROM tbl_empresas_practica AS ep
@@ -28,7 +31,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '')";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         public function listar_fechas($fecha_inicio,$fecha_fin)
         {
@@ -45,7 +48,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '')  AND ep.Fecha_creacion BETWEEN '$fecha_inicio' AND '$fecha_fin'";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         public function listar_fechas_docente($fecha_inicio,$fecha_fin,$docente)
         {
@@ -62,7 +65,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '')  AND ep.Fecha_creacion BETWEEN '$fecha_inicio' AND '$fecha_fin' and pe.docente_supervisor='$docente' ";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         public function listar_empresa($empresa)
         {
@@ -79,7 +82,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '')  AND ep.nombre_empresa='$empresa'";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         public function listar_docente($docente)
         {
@@ -96,7 +99,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '') and pe.docente_supervisor='$docente'";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         public function listar_fechas_docente_empresa($fecha_inicio,$fecha_fin,$empresa,$docente)
         {
@@ -113,7 +116,7 @@ class Estadisticas
                 JOIN tbl_tipo_contactos f ON e.id_tipo_contacto = f.id_tipo_contacto AND f.descripcion = 'TELEFONO CELULAR'
                 join tbl_personas_extendidas as px on px.id_atributo=12 and px.id_persona=a.id_persona
                 WHERE NOT (pe.docente_supervisor <=> '')  AND pe.docente_supervisor='$docente' AND ep.nombre_empresa='$empresa' AND ep.Fecha_creacion BETWEEN '$fecha_inicio' AND '$fecha_fin'";
-                return ejecutarConsulta($sql);
+                return $instancia_conexion->ejecutarConsulta($sql);
         }
         
         
