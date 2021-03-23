ef334a81caa35294fd5a65aa825e3188f1ffe652
Daniel <r1912ramirez@gmail.com>
2021-03-23 16:00:28 -0600

    ruta nueva

--- api/asistencia_charla_api.php
+++ api/asistencia_charla_api.php
@@ -41,9 +41,7 @@ elseif (isset($_GET["asistencia"]))
 
      //header("location:../vistas/gestion_asistencia_charla_vista.php?msj=1"); 
 	 echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=1'); </script>";
-	 ?>
-	 <meta http-equiv="refresh" content="10; url=https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=1">
-    <?php
+	
 	/*echo '<script> alert("  '.$identificador.'  ")</script>';*/
 
 	}	
@@ -80,7 +78,8 @@ elseif (isset($_GET["asistencia"]))
 		 bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'ACTUALIZO' , 'LA ASISTENCIA CHARLA.');
      		 
 		 //header("location: https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=2'"); 
-		 echo "<script type='text/javascript'> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=2'); </script>";
+		 echo "<script> window.location = 'https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=2';</script>";
+		// echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/gestion_documentos_practica_vista.php?msj=1'); </script>";
 		// header("Location: https://www.informaticaunah.com/automatizacion/vistas/gestion_asistencia_charla_vista.php?msj=2", TRUE, 301);
 	
 			}	
