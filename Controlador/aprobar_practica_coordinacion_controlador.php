c167838ccd0ab7223d40e83a956f0ed95ce3ca9c
Daniel <r1912ramirez@gmail.com>
2021-04-19 20:08:45 -0600

    aprobacion pps

--- Controlador/aprobar_practica_coordinacion_controlador.php
+++ Controlador/aprobar_practica_coordinacion_controlador.php
@@ -4,21 +4,24 @@ session_start();
 require_once ('../clases/Conexion.php');
 require_once ('../clases/funcion_bitacora.php');
 
-if (isset($_POST['cb_practica']))
-{
 
-	$_SESSION['cb_practica']=strtoupper ($_POST['cb_practica']);
+
+
+	    $_SESSION['cb_practica']=strtoupper ($_POST['cb_practica']);
 		$_SESSION['cb_horas_practica']=strtoupper ($_POST['cb_horas_practica']);
+		$_SESSION['fecha_inicio']=strtoupper ($_POST['fecha_inicio']);
+		$_SESSION['fecha_finalizacion']=strtoupper ($_POST['fecha_finalizacion']);
+		
+	print_r($_POST);
 
-
-	$_SESSION['txt_estudiante_cuenta']=strtoupper ($_POST['txt_estudiante_cuenta']);
+	/*$_SESSION['txt_estudiante_cuenta']=strtoupper ($_POST['txt_estudiante_cuenta']);
 	$_SESSION['empresa']=strtoupper ($_POST['txt_empresa']);
 
 //Cuando practica es sin seleccionar
 	if ($_SESSION['cb_practica']=='0' and $_SESSION['cb_horas_practica']=='0')
 	{
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=1"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
 
 	}
 	//Cuando aprobar y practica es SI
@@ -29,7 +32,7 @@ if (isset($_POST['cb_practica']))
 $_SESSION['txt_motivo']="APROBADA";
 
 // tipo 1 de aprobado
-		$sql = "call proc_aprobacion_practica($_SESSION[txt_estudiante_cuenta],' $_SESSION[txt_motivo]',1,'	$_SESSION[empresa]', '$_SESSION[cb_horas_practica]') ";
+		$sql = "call proc_aprobacion_practica($_SESSION[txt_estudiante_cuenta],' $_SESSION[txt_motivo]',1,'	$_SESSION[empresa]', '$_SESSION[cb_horas_practica]', '$_SESSION[fecha_inicio]', '$_SESSION[fecha_finalizacion]') ";
 
 		$resultado = $mysqli->query($sql);
 		if ($resultado==true) {
@@ -37,13 +40,13 @@ $_SESSION['txt_motivo']="APROBADA";
 			      bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'INGRESO' , 'COORDINACION DEL ESTUDIANTE CON CUENTA'.$_SESSION['txt_estudiante_cuenta'] .' CON DECISION '.$_SESSION['cb_aprobar']. ', CON OBSERVACION'.$_SESSION['txt_motivo'].'');  
 
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=2"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
 
 		}
 		else
 		{
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=3"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
 
 		}
 
@@ -57,7 +60,7 @@ $_SESSION['txt_motivo']="APROBADA";
 		 {
 		 	//valida motivo vacio
 		 	//header("location:../vistas/gestion_documentos_practica_vista.php?msj=1"); 
-			 echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
+			 echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
 
 		}
 		else
@@ -72,14 +75,14 @@ $_SESSION['txt_motivo']="APROBADA";
 			      bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'INGRESO' , 'COORDINACION DEL ESTUDIANTE CON CUENTA'.$_SESSION['txt_estudiante_cuenta'] .' CON DECISION '.$_SESSION['cb_aprobar']. ', CON OBSERVACION'.$_SESSION['txt_motivo'].'');  
 
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=2"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
 
 
 		}
 		else
 		{
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=3"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
 
 		}
 		}
@@ -99,7 +102,7 @@ $_SESSION['txt_motivo']="APROBADA";
 		 {
 		 	//valida motivo vacio
 		 	//header("location:../vistas/gestion_documentos_practica_vista.php?msj=1"); 
-			 echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
+			 echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=1'); </script>";
 
 		}
 		else
@@ -114,20 +117,20 @@ $_SESSION['txt_motivo']="APROBADA";
 			      bitacora::evento_bitacora($Id_objeto, $_SESSION['id_usuario'],'INGRESO' , 'COORDINACION DEL ESTUDIANTE CON CUENTA'.$_SESSION['txt_estudiante_cuenta'] .' CON DECISION '.$_SESSION['cb_aprobar']. ', CON OBSERVACION'.$_SESSION['txt_motivo'].'');  
 
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=2"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=2'); </script>";
 
 
 		}
 		else
 		{
 		//header("location:../vistas/aprobar_practica_coordinacion_vista.php?msj=3"); 
-		echo "<script> window.location.replace('https://www.informaticaunah.com/automatizacion/vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
+		echo "<script> window.location.replace('../vistas/aprobar_practica_coordinacion_vista.php?msj=3'); </script>";
 
 		}		}
 
 	
-	 }
-	 }
+	 }*/
+	 
 	
 
 
