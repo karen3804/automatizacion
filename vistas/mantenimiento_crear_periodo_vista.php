8f212aae9a19a17cb0b13e7eee75414d2d7f017b
LPACHECO10 <72700734+LPACHECO10@users.noreply.github.com>
2021-05-02 21:34:21 -0600

    permisos periodo
    id objeto periodo 55

--- vistas/mantenimiento_crear_periodo_vista.php
+++ vistas/mantenimiento_crear_periodo_vista.php
@@ -42,7 +42,7 @@ if ($visualizacion == 0) {
 
 
 
-    if (permisos::permiso_insertar($Id_objeto) == '1') {
+    if (permisos::permiso_insertar($Id_objeto) == '55') {
         $_SESSION['btn_guardar_periodo'] = "";
     } else {
         $_SESSION['btn_guardar_periodo'] = "disabled";
